f71d82940d8ffe8a4d2e579033b20df989200e3d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-04-25 22:21:51 -0300

    Refactory function pegaArquivo promises

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import chalk from 'chalk'
+import fs from 'fs'
 
-console.log(chalk.red("vamos começar"))
-
-const paragrafo = 'Texto retornado por uma função'
-
-function texto(string) {
-    return string
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
-console.log(texto(paragrafo))
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.white(texto)))
+    .catch((erro) => trataErro(erro))
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
+//         if(erro) {
+//             trataErro(erro)
+//         }
+//         console.log(chalk.white(data))
+//     })
+// }
+
+pegaArquivo('./arquivos/texto1.md')
