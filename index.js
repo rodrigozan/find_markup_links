b9f338cac93676074ae4aad79e3cc0e3fda2ec35
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-04-25 21:43:48 -0300

    Instalando as primeiras dependências

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+
+console.log(chalk.red("vamos começar"))
+
+const paragrafo = 'Texto retornado por uma função'
+
+function texto(string) {
+    return string
+}
+
+console.log(texto(paragrafo))
