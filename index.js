b2470e84ec51f946fde463d25f98d9fc32a9d20e
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-07-05 10:30:51 -0300

    Async / Await

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.white(texto)))
-    .catch((erro) => trataErro(erro))
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.white(texto))
+    } catch (erro) {
+        trataErro(erro)
+    }
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.white(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+
 
 // function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8'
