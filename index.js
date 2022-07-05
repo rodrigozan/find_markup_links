f1b2772581008fb30eb2dc0bafbc06a4359dfd14
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-07-05 11:03:12 -0300

    Test regular expressions in texto1.md

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import chalk from 'chalk'
 import fs from 'fs'
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s].[^\s]*)\)/gm
+    const linksExtraidos = texto.match(regex)
+    console.log(linksExtraidos)
+}
+
+extraiLinks(texto)
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
-async function pegaArquivo(caminhoDoArquivo) {
-    try {
-        const encoding = 'utf-8'
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.white(texto))
-    } catch (erro) {
-        trataErro(erro)
-    }
-}
+// async function pegaArquivo(caminhoDoArquivo) {
+//     try {
+//         const encoding = 'utf-8'
+//         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+//         console.log(chalk.white(texto))
+//     } catch (erro) {
+//         trataErro(erro)
+//     }
+// }
 
 // function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8'
@@ -35,4 +45,4 @@ async function pegaArquivo(caminhoDoArquivo) {
 //     })
 // }
 
-pegaArquivo('./arquivos/texto1.md')
+//pegaArquivo('./arquivos/texto1.md')
